78e15d53e31a5498c6253628a21f8c130f883fc5
Stanley Yu <stantyu9@gmail.com>
2024-08-20 02:20:05 -0700

    Updates

---
+++ precache-manifest.fdb1274ba2f4de6a4a5bcff771e16cbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7aea86774d1000819429",
+    "url": "/stanley-yu.com/static/css/main.d336eabe.chunk.css"
+  },
+  {
+    "revision": "7aea86774d1000819429",
+    "url": "/stanley-yu.com/static/js/main.7aea8677.chunk.js"
+  },
+  {
+    "revision": "143a9a6282013744e9c2",
+    "url": "/stanley-yu.com/static/js/1.143a9a62.chunk.js"
+  },
+  {
+    "revision": "3d0c0b8706bb0c6a048e",
+    "url": "/stanley-yu.com/static/js/runtime~main.3d0c0b87.js"
+  },
+  {
+    "revision": "bfbbf0ae0817a8240451c9cd3a62bad2",
+    "url": "/stanley-yu.com/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f235802b26c69a579999a1f27436a220.js"
+  "/stanley-yu.com/precache-manifest.fdb1274ba2f4de6a4a5bcff771e16cbe.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/stanley-yu.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
